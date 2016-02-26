7daaf95555c88b5ceeedf0b6dd9ccf9e954fe746
TuongNguyen <vantuong6894@gmail.com>
2016-02-26 09:20:13 +0700

    edit image slider

---
+++ GoogleMapTest/app/src/main/java/com/hvngoc/googlemaptest/activity/CommentActivity.java
@@ -0,0 +1,13 @@
+package com.hvngoc.googlemaptest.activity;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommentActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- GoogleMapTest/app/src/main/java/com/hvngoc/googlemaptest/activity/NewsDetailActivity.java
+++ GoogleMapTest/app/src/main/java/com/hvngoc/googlemaptest/activity/NewsDetailActivity.java
@@ -1,29 +1,40 @@
 package com.hvngoc.googlemaptest.activity;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.TypedArray;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.Gallery;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.hvngoc.googlemaptest.R;
-import com.hvngoc.googlemaptest.adapter.RVCommentAdapter;
 
-public class NewsDetailActivity extends BaseActivity {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+
+public class NewsDetailActivity extends BaseActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener{
 
     private ImageView imageView;
+    private SliderLayout mDemoSlider;
+    private Button commentBtn;
+    private PopupWindow popWindow;
 
     //the images to display
     Integer[] imageIDs = {
@@ -39,53 +50,98 @@ public class NewsDetailActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        commentBtn = (Button) findViewById(R.id.commentBtn);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Global.CurentContext = this;
-
-        ImageView imgShowMap = (ImageView) findViewById(R.id.imgViewShowMap);
-        imgShowMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Global.CurentContext, MapsActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                Global.CurentContext.startActivity(intent);
-            }
-        });
-
-        imageView = (ImageView) findViewById(R.id.image1);
-        Gallery gallery = (Gallery) findViewById(R.id.gallery);
-        gallery.setAdapter(new ImageAdapter(this));
-        gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                imageView.setImageResource(imageIDs[position]);
-            }
-        });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabViewComment);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar.make(v, "fragment recycle view all comment here", Snackbar.LENGTH_LONG)
-                .setAction("Replace with your own action", null).show();
-            }
-        });
-
+        mDemoSlider = (SliderLayout)findViewById(R.id.slider);
         getNewsDetailData();
 
-        SetCommentDetail();
+        HashMap<String,String> url_maps = new HashMap<String, String>();
+        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+
+        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
+        file_maps.put("Hannibal",R.drawable.hannibal);
+        file_maps.put("Big Bang Theory",R.drawable.bigbang);
+        file_maps.put("House of Cards",R.drawable.house);
+        file_maps.put("Game of Thrones", R.drawable.game_of_thrones);
+
+        for(String name : file_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(this);
+            // initialize a SliderLayout
+            textSliderView
+                    .image(file_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra", name);
+
+            mDemoSlider.addSlider(textSliderView);
+        }
+
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+        mDemoSlider.setDuration(4000);
+        mDemoSlider.addOnPageChangeListener(this);
+
+        commentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onShowPopup(v);
+            }
+        });
     }
 
-    private void SetCommentDetail(){
-        RecyclerView mRecyclerView = (RecyclerView)findViewById(R.id.recyclerListComment);
-        mRecyclerView.setHasFixedSize(true);
+    public void onShowPopup(View v){
 
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        LayoutInflater layoutInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        RecyclerView.Adapter mAdapter = new RVCommentAdapter();
-        mRecyclerView.setAdapter(mAdapter);
+        // inflate the custom popup layout
+        final View inflatedView = layoutInflater.inflate(R.layout.popup_comment_layout, null,false);
+        // find the ListView in the popup layout
+        ListView listView = (ListView)inflatedView.findViewById(R.id.commentsListView);
+
+        // get device size
+        Display display = getWindowManager().getDefaultDisplay();
+        final Point size = new Point();
+        display.getSize(size);
+        //mDeviceHeight = size.y;
+
+
+        // fill the data to the list items
+        setSimpleList(listView);
+
+
+        // set height depends on the device size
+        popWindow = new PopupWindow(inflatedView, size.x - 50,size.y - 400, true );
+        // set a background drawable with rounders corners
+        //popWindow.setBackgroundDrawable(getResources().getDrawable(R.drawable.fb_popup_bg));
+        // make it focusable to show the keyboard to enter in `EditText`
+        popWindow.setFocusable(true);
+        // make it outside touchable to dismiss the popup window
+        popWindow.setOutsideTouchable(true);
+
+        // show the popup at bottom of the screen and set some margin at bottom ie,
+        popWindow.showAtLocation(v, Gravity.BOTTOM, 0, 100);
+    }
+
+    void setSimpleList(ListView listView){
+
+        ArrayList<String> contactsList = new ArrayList<String>();
+
+        for (int index = 0; index < 10; index++) {
+            contactsList.add("I am @ index " + index + " today " + Calendar.getInstance().getTime().toString());
+        }
+
+        listView.setAdapter(new ArrayAdapter<String>(NewsDetailActivity.this,
+                R.layout.comment_list_items, android.R.id.text1,contactsList));
     }
 
     private void getNewsDetailData(){
@@ -119,37 +175,19 @@ public class NewsDetailActivity extends BaseActivity {
         return R.layout.activity_detail_news;
     }
 
-    public class ImageAdapter extends BaseAdapter {
-        private Context context;
-        private int itemBackground;
-        public ImageAdapter(Context c)
-        {
-            context = c;
-            // sets a grey background; wraps around the images
-            TypedArray a =obtainStyledAttributes(R.styleable.MyGallery);
-            itemBackground = a.getResourceId(R.styleable.MyGallery_android_galleryItemBackground, 0);
-            a.recycle();
-        }
-        // returns the number of images
-        public int getCount() {
-            return imageIDs.length;
-        }
-        // returns the ID of an item
-        public Object getItem(int position) {
-            return position;
-        }
-        // returns the ID of an item
-        public long getItemId(int position) {
-            return position;
-        }
-        // returns an ImageView view
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageView = new ImageView(context);
-            imageView.setImageResource(imageIDs[position]);
-            imageView.setLayoutParams(new Gallery.LayoutParams(100, 100));
-            imageView.setBackgroundResource(itemBackground);
-            return imageView;
-        }
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {}
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+    @Override
+    public void onPageSelected(int position) {
+        Log.d("Slider Demo", "Page Changed: " + position);
     }
 
 }
