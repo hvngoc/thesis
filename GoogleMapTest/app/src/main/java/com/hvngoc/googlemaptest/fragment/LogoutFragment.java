16090fb160892d7481dc98f14e119f34da3f306e
hvngoc <hvngoc94@gmail.com>
2016-04-01 10:34:41 +0700

    setting fragment

--- GoogleMapTest/app/src/main/java/com/hvngoc/googlemaptest/fragment/LogoutFragment.java
+++ GoogleMapTest/app/src/main/java/com/hvngoc/googlemaptest/fragment/LogoutFragment.java
@@ -1,9 +1,7 @@
 package com.hvngoc.googlemaptest.fragment;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
