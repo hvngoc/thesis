fe89b7b7fadd1464e63ba874a2af7ac52e8696bb
hvngoc <hvngoc94@gmail.com>
2016-04-08 11:45:34 +0700

    search friend by email

--- GoogleMapTest/app/src/main/java/com/hvngoc/googlemaptest/fragment/FriendFindFragment.java
+++ GoogleMapTest/app/src/main/java/com/hvngoc/googlemaptest/fragment/FriendFindFragment.java
@@ -3,15 +3,19 @@ package com.hvngoc.googlemaptest.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.hvngoc.googlemaptest.R;
 
 public class FriendFindFragment extends Fragment {
 
-
+    CardView cardFindFriend;
+    ImageView btnFriendSearch;
     public FriendFindFragment() {
         // Required empty public constructor
     }
@@ -26,6 +30,16 @@ public class FriendFindFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_friend_find, container, false);
+        cardFindFriend = (CardView) view.findViewById(R.id.cardFindFriend);
+        cardFindFriend.setVisibility(View.INVISIBLE);
+
+        btnFriendSearch = (ImageView) view.findViewById(R.id.btnFriendSearch);
+        btnFriendSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //send query and then set to card view
+            }
+        });
         return view;
     }
 
