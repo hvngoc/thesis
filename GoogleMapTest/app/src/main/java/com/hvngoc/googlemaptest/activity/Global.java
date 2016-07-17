0d9d65f61da2936568f8033513960c75456367be
hvngoc <hvngoc94@gmail.com>
2016-07-17 20:50:14 +0700

    set release

--- GoogleMapTest/app/src/main/java/com/hvngoc/googlemaptest/activity/Global.java
+++ GoogleMapTest/app/src/main/java/com/hvngoc/googlemaptest/activity/Global.java
@@ -28,8 +28,8 @@ public class GLOBAL {
 
     //public static final String SERVER_URL = "http://10.0.3.2:9000/api/";
     //public static final String SERVER_IMAGE_URL = "http://10.0.3.2:9000/images/";
-    public static final String SERVER_URL = "http://ae4c6ae5.ngrok.io/api/";
-    public static final String SERVER_IMAGE_URL = "http://ae4c6ae5.ngrok.io/images/";
+    public static final String SERVER_URL = "http://d5beee9c.ngrok.io/api/";
+    public static final String SERVER_IMAGE_URL = "http://d5beee9c.ngrok.io/images/";
 
     public static HashMap<String, String> NOTIFICATION = null;
 
